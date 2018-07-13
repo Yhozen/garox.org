87e0376dd44488ae52b4e9cfb6b2c35ed9eeb634
garox <gabriel@garox.org>
2018-07-12 23:29:02 -0400

    avances

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,9 @@ import { withStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button'
 
 const styles = theme => ({
@@ -15,32 +18,61 @@ const styles = theme => ({
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
 });
+
+const Me = (props) => {
+  const { classes } = props
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.media}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="Contemplative Reptile"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="headline" component="h2">
+          Lizard
+        </Typography>
+        <Typography component="p">
+          Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+          across all continents except Antarctica
+        </Typography>
+      </CardContent>
+    </Card>
+  )
+}
 
 const IndexPage = (props) => {
   const { classes } = props
-
   return (
-    <div>
-      <Grid container justify="center" direction='column' >
-        <Grid item xs={12}>
-          <Grid container spacing={24} justify="center">
-            {[0, 1, 2].map(value => (
-              <Grid key={value} item xs={4}>
-                <Paper className={classes.paper} elevation={2}>
-                <Typography variant="headline" component="h3">
-                  This is a sheet of paper.
-                </Typography>
-                <Typography component="p">
-                  Paper can be used to build surface or other elements for your application.
-                </Typography>
-                </Paper>
-              </Grid>
-            ))}
-          </Grid>
+    <Grid container justify='center' alignItems='center' direction='column' style={{ padding: 20 }}>
+      <Grid item xs={12}>
+        <Me classes={classes}/>
+      </Grid>
+      <Grid item xs={12} style={{ padding: 20 }}>
+        <Grid container spacing={24} justify="center">
+          {[0, 1, 2].map(value => (
+            <Grid key={value} item sm={4}>
+              <Paper className={classes.paper} elevation={2}>
+              <Typography variant="headline" component="h3">
+                This is a sheet of paper.
+              </Typography>
+              <Typography component="p">
+                Paper can be used to build surface or other elements for your application.
+              </Typography>
+              </Paper>
+            </Grid>
+          ))}
         </Grid>
       </Grid>
-    </div>
+    </Grid>
   )
 }
 
