fee134f26411edd4484c6102ed2c02048024ca81
garox <gabriel@garox.org>
2018-07-13 16:01:22 -0400

    texto

---
+++ src/components/Me.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
+import green from '@material-ui/core/colors/green'
+import Typography from '@material-ui/core/Typography'
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import Dayjs from 'dayjs'
+
+const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: green[500]
+      }
+    }
+  })
+
+const now = Dayjs()
+const nacimiento = Dayjs('1998-07-28')
+const años = now.diff(nacimiento, 'years', true)
+
+const Me = (props) => {
+  const { classes } = props
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.media}
+        image="https://avatars1.githubusercontent.com/u/6902134"
+        title="Contemplative Reptile"
+      />
+      <CardContent>
+        <Typography variant="headline" component="h2">
+          Gabriel Pérez
+        </Typography>
+        <Typography gutterBottom variant="subheading" component="h5">
+          {años | 0} years old
+        </Typography>
+        <MuiThemeProvider theme={theme}>
+          <LinearProgress variant="determinate" value={(años % 1)*100 } style={{marginBottom: '1em'}}/>
+        </MuiThemeProvider>
+        <Typography component="p">
+          A programmer orem ipsum
+ dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+        </Typography>
+        
+      </CardContent>
+    </Card>
+  )
+}
+
+export default Me
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,10 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import { withStyles, createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
-import green from '@material-ui/core/colors/green'
+import { withStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent'
-import CardMedia from '@material-ui/core/CardMedia'
-import LinearProgress from '@material-ui/core/LinearProgress'
-import Dayjs from 'dayjs'
-import Tooltip from '@material-ui/core/Tooltip'
+import Me from '../components/Me'
 
 const styles = theme => ({
   root: {
@@ -29,46 +23,6 @@ const styles = theme => ({
     paddingTop: '85%',
   }
 })
-const theme = createMuiTheme({
-  palette: {
-    primary: {
-      main: green[500]
-    }
-  }
-})
-const now = Dayjs()
-const nacimiento = Dayjs('1998-07-28')
-const años = now.diff(nacimiento, 'years', true)
-console.log(green[400])
-
-const Me = (props) => {
-  const { classes } = props
-  return (
-    <Card className={classes.card}>
-      <CardMedia
-        className={classes.media}
-        image="https://avatars1.githubusercontent.com/u/6902134"
-        title="Contemplative Reptile"
-      />
-      <CardContent>
-        <Typography variant="headline" component="h2">
-          Gabriel Pérez
-        </Typography>
-        <Typography gutterBottom variant="subheading" component="h5">
-          {años | 0} years old
-        </Typography>
-        <MuiThemeProvider theme={theme}>
-          <LinearProgress variant="determinate" value={(años % 1)*100 } style={{marginBottom: '1em'}}/>
-        </MuiThemeProvider>
-        <Typography component="p">
-          A programmer orem ipsum
- dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
-        </Typography>
-        
-      </CardContent>
-    </Card>
-  )
-}
 
 const IndexPage = (props) => {
   const { classes } = props
@@ -86,7 +40,8 @@ const IndexPage = (props) => {
                 This is a sheet of paper.
               </Typography>
               <Typography component="p">
-              Lorem ipsum. At vero eos et accusam et justo duo dolores et ea rebum.              </Typography>
+                Lorem ipsum. At vero eos et accusam et justo duo dolores et ea rebum.              
+              </Typography>
               </Paper>
             </Grid>
           ))}
