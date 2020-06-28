d546f34c55fa06b1c4ee70c2a4edc64a61485e13
Gabriel Pérez <garox@pop-os.localdomain>
2020-06-28 14:24:37 -0400

    update gatsby

--- src/components/Me.js
+++ src/components/Me.js
@@ -1,28 +1,28 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import Link from 'gatsby-link'
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
 import green from '@material-ui/core/colors/green'
 import Typography from '@material-ui/core/Typography'
-import Card from '@material-ui/core/Card';
+import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
 import CardMedia from '@material-ui/core/CardMedia'
 import LinearProgress from '@material-ui/core/LinearProgress'
 import Dayjs from 'dayjs'
 
 const theme = createMuiTheme({
-    palette: {
-      primary: {
-        main: green[500]
-      }
-    }
-  })
+  palette: {
+    primary: {
+      main: green[500],
+    },
+  },
+})
 
-const now = Dayjs()
 const nacimiento = Dayjs('1998-07-28')
-const años = now.diff(nacimiento, 'years', true)
 
 const Me = (props) => {
   const { classes } = props
+  const now = useMemo(() => Dayjs(), [])
+  const años = useMemo(() => now.diff(nacimiento, 'years', true), [now])
   return (
     <Card className={classes.card}>
       <CardMedia
@@ -38,13 +38,18 @@ const Me = (props) => {
           {años | 0} years old
         </Typography>
         <MuiThemeProvider theme={theme}>
-          <LinearProgress variant="determinate" value={(años % 1)*100 } style={{marginBottom: '1em'}}/>
+          <LinearProgress
+            variant="determinate"
+            value={(años % 1) * 100}
+            style={{ marginBottom: '1em' }}
+          />
         </MuiThemeProvider>
         <Typography component="p">
-          A programmer orem ipsum
- dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+          A programmer orem ipsum dolor sit amet, consetetur sadipscing elitr,
+          sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+          aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo
+          dolores et ea rebum.
         </Typography>
-        
       </CardContent>
     </Card>
   )
