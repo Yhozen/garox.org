55c3db6bff20d9c0366f867bead68c2a6158c0d3
Gabriel Pérez <gabriel@garox.org>
2021-07-29 16:15:28 -0400

    prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import {  StyledComponentProps, withStyles } from '@material-ui/core/styles'
+import { StyledComponentProps, withStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
@@ -21,10 +21,10 @@ const materialStyles = (theme: any) => ({
   media: {
     height: 0,
     paddingTop: '85%',
-  }
+  },
 })
 
-const Home  = ({ classes }: StyledComponentProps) =>{
+const Home = ({ classes }: StyledComponentProps) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -33,27 +33,34 @@ const Home  = ({ classes }: StyledComponentProps) =>{
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Grid container justify='center' alignItems='center' direction='column' style={{ padding: 20 }}>
-      <Grid item xs={12}>
-        <Me classes={classes}/>
-      </Grid>
-      <Grid item xs={12} style={{ padding: 20 }}>
-        <Grid container spacing={24} justify="center">
-          {[0, 1, 2].map(value => (
-            <Grid key={value} item sm={4}>
-              <Paper className={classes?.paper} elevation={2}>
-              <Typography variant="headline" component="h3">
-                This is a sheet of paper.
-              </Typography>
-              <Typography component="p">
-                Lorem ipsum. At vero eos et accusam et justo duo dolores et ea rebum.              
-              </Typography>
-              </Paper>
-            </Grid>
-          ))}
+      <Grid
+        container
+        justify="center"
+        alignItems="center"
+        direction="column"
+        style={{ padding: 20 }}
+      >
+        <Grid item xs={12}>
+          <Me classes={classes} />
+        </Grid>
+        <Grid item xs={12} style={{ padding: 20 }}>
+          <Grid container spacing={24} justify="center">
+            {[0, 1, 2].map((value) => (
+              <Grid key={value} item sm={4}>
+                <Paper className={classes?.paper} elevation={2}>
+                  <Typography variant="headline" component="h3">
+                    This is a sheet of paper.
+                  </Typography>
+                  <Typography component="p">
+                    Lorem ipsum. At vero eos et accusam et justo duo dolores et
+                    ea rebum.
+                  </Typography>
+                </Paper>
+              </Grid>
+            ))}
+          </Grid>
         </Grid>
       </Grid>
-    </Grid>
     </div>
   )
 }
