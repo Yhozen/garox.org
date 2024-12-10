379ae353ad7e5d893287ee76e023822283ae44ea
Gabriel Pérez <gabriel@garox.org>
2024-12-10 11:57:12 -0300

    use nextjs app router

--- hello.ts
+++ hello.ts
@@ -1,4 +1,5 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+'use server'
+
 import fetch from 'isomorphic-fetch'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
